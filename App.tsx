42405b16c706b0a5d2d15f6dd169508601b8a394
Devessier <baptiste@devessier.fr>
2021-02-10 23:10:54 +0100

    feat: create several boxes of different colours and style them

--- App.tsx
+++ App.tsx
@@ -4,15 +4,42 @@ import { Text, View, SafeAreaView } from 'react-native';
 import tw from 'tailwind-rn';
 
 export default function App() {
+  const boxes = [
+    {
+      style: tw('bg-green-400'),
+      text: 'Light green',
+    },
+    {
+      style: tw('bg-blue-500'),
+      text: 'Blue',
+    },
+    {
+      style: tw('bg-pink-600'),
+      text: 'Pink',
+    },
+    {
+      style: tw('bg-yellow-500'),
+      text: 'Orange',
+    },
+  ];
+
   return (
     <SafeAreaView style={tw('flex-1')}>
-      <View
-        style={tw(
-          'bg-yellow-500 text-white p-4 flex-1 items-center justify-center',
-        )}
-      >
-        <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={tw('flex-1 p-4')}>
         <StatusBar style="auto" />
+
+        <Text style={tw('font-bold text-base mb-2')}>
+          Here are somes boxes of different colours
+        </Text>
+
+        {boxes.map(({ style, text }) => (
+          <View
+            key={text}
+            style={[style, tw('justify-center items-center py-2 mb-2')]}
+          >
+            <Text style={tw('text-white')}>{text}</Text>
+          </View>
+        ))}
       </View>
     </SafeAreaView>
   );
