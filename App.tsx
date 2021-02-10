510046b2b083d1cf734ceca7fe5e128b3c2d93fa
Devessier <baptiste@devessier.fr>
2021-02-10 23:00:38 +0100

    feat: use tailwind-rn to style components using Tailwind classes

--- App.tsx
+++ App.tsx
@@ -1,33 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
+import { Text, View, SafeAreaView } from 'react-native';
+import tw from 'tailwind-rn';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={[styles.container, styles.orange]}>
+    <SafeAreaView style={tw('flex-1')}>
+      <View
+        style={tw(
+          'bg-yellow-500 text-white p-4 flex-1 items-center justify-center',
+        )}
+      >
         <Text>Open up App.tsx to start working on your app!</Text>
         <StatusBar style="auto" />
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-  },
-
-  orange: {
-    backgroundColor: 'orange',
-    color: 'white',
-  },
-
-  container: {
-    padding: 10,
-
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
